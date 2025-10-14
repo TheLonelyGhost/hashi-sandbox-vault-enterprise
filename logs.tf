f437021ce74b6d58a1d59cdbd8e552b6f5ec4c4f
David Alexander <opensource@thelonelyghost.com>
2025-10-14 18:23:15 -0400

    Vault logs as file

--- logs.tf
+++ logs.tf
@@ -16,3 +16,20 @@ resource "vault_generic_endpoint" "audit_logs" {
     }
   )
 }
+
+resource "vault_generic_endpoint" "audit_log_file" {
+  path         = "sys/audit/file"
+  disable_read = true
+
+  data_json = jsonencode({
+    type                 = "file"
+    description          = "Logs to file"
+    elide_list_responses = true
+
+    options = {
+      file_path = "/var/log/vault/audit.log"
+    }
+
+    log_raw = true
+  })
+}
